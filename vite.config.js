3285d098c654da20dbb40ceb193921a47ac9bd81
N-eeraj <neeraj7rajeena@gmail.com>
2023-05-29 22:51:17 +0530

    setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/tftf/' : '/',
   plugins: [react()],
   resolve:{
     alias: {
