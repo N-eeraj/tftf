d4e4aa336123559b5d194644849d15f9f9df6cb9
N-eeraj <neeraj7rajeena@gmail.com>
2024-06-19 12:21:34 +0530

    Bring cars to stop in order on race completion

--- src/components/game/Canvas.jsx
+++ src/components/game/Canvas.jsx
@@ -25,7 +25,7 @@ const Canvas = ({ peerId, connections, started }) => {
     ctx.fillStyle = '#2E2E2E'
     ctx.fillRect(0, 0, canvas.current.width, canvas.current.height)
 
-    lineUp.current?.forEach(({ index, progress, carPosition, completed }, lineUpIndex) => {
+    lineUp.current?.forEach(({ key, index, progress, carPosition, completed, lastUpdated }, lineUpIndex) => {
       ctx.fillStyle = '#EEE'
 
       const sx = 0
@@ -34,20 +34,39 @@ const Canvas = ({ peerId, connections, started }) => {
       const sHeight = cars.current.height / 8
       const dHeight = trackHeight * 0.75
       const dWidth = dHeight * 1.913
-      const dx = carPosition
+      let dx = carPosition
       const dy = trackHeight * (index + 0.125)
 
-      const gameOver = lineUp.current.find(({ key }) => key === peerId).progress === 1
+      const self = lineUp.current.find(({ key }) => key === peerId)
+      const gameOver = self.progress === 1
       if (gameOver) {
         dashes.current.forEach(progress => 
           ctx.fillRect(progress, trackHeight * (index + 0.5) - dashesConfig.height * 0.5, dashesConfig.width, dashesConfig.height)
         )
-        if (completed && carPosition < canvas.current.width / 2) {
-          lineUp.current[lineUpIndex].carPosition += 2
+        if (key === peerId) {
+          if (completed && carPosition < canvas.current.width / 2) {
+            lineUp.current[lineUpIndex].carPosition += 2
+          }
+          else if (!completed) {
+            lineUp.current[lineUpIndex].completed = true
+            lineUp.current[lineUpIndex].carPosition = -dWidth
+          }
         }
-        else if (!completed) {
-          lineUp.current[lineUpIndex].completed = true
-          lineUp.current[lineUpIndex].carPosition = -dWidth
+        else if (progress === 1) {
+          if (self.lastUpdated > lastUpdated) {
+            lineUp.current[lineUpIndex].completed = true
+            dx = canvas.current.width / 2
+          }
+          else {
+            if (completed && carPosition < canvas.current.width / 2) {
+              lineUp.current[lineUpIndex].carPosition += 2
+            }
+            else if (!completed) {
+              lineUp.current[lineUpIndex].completed = true
+              lineUp.current[lineUpIndex].carPosition = -dWidth
+            }
+          }
+          ctx.drawImage(cars.current, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
         }
       }
       else {
@@ -66,12 +85,14 @@ const Canvas = ({ peerId, connections, started }) => {
           if (!progress && carPosition < canvas.current.width * 0.05) {
             lineUp.current[lineUpIndex].carPosition += 1
           }
-          else if (carPosition < canvas.current.width * +progress.toFixed(1) - dWidth) {
+          else if (carPosition < canvas.current.width * +progress.toFixed(1) - dWidth || progress === 1) {
             lineUp.current[lineUpIndex].carPosition += 0.5
           }
         }
       }
-      ctx.drawImage(cars.current, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+      if (!gameOver || key === peerId) {
+        ctx.drawImage(cars.current, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+      }
     })
   }
 
@@ -79,13 +100,16 @@ const Canvas = ({ peerId, connections, started }) => {
     canvas.current.height = Object.keys(connections).length * 40
     canvas.current.width = 600
     lineUp.current = Object.entries(connections).reduce((sorted, [key, { index, ...progress }], connectionsIndex) => {
-      let carPosition = 0
-      if (lineUp.current)
+      let carPosition, completed
+      if (lineUp.current) {
         carPosition = lineUp.current[connectionsIndex]?.carPosition ?? 0
+        completed = lineUp.current[connectionsIndex]?.completed ?? false
+      }
       const data = {
         key,
         index,
         carPosition,
+        completed,
         ...progress,
       }
       if (!sorted.length || index < sorted[0].index)
